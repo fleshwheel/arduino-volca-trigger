34b47b0f04608b391a7d2afcb78d7a47eecaf79e
Liam Schumm <lschumm@pm.me>
2023-07-21 20:29:37 -0500

    Bonk,. bonk. bonk. bonk. bonk. bonk. bonk. bonk.;

---
+++ README.md
@@ -0,0 +1,3 @@
+# avr-volca-trigger
+
+Simple Arduino script to convert line-level audio trigger pulses to sync pulses compatible with Korg's Volca synthesizers. I wrote this script in order to synchronize Korg DS-10 running on my Nintendo DS to my Volca Sample 2.
---
+++ main.ino
@@ -0,0 +1,53 @@
+// register set/clear macros
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+// voltage reference for ADC
+#define AREF INTERNAL
+
+// threshold at/above which we trigger a pulse
+#define ATHRESH 6
+
+// pulse output pin
+#define POUT 5
+
+inline int sample() {  
+  // start the conversion
+  sbi(ADCSRA, ADSC);
+
+  // ADSC is cleared when the conversion finishes
+  while (bit_is_set(ADCSRA, ADSC));
+
+  // ADC macro takes care of reading ADC register.
+  // avr-gcc implements the proper reading order: ADCL is read first.
+  return ADC;
+}
+
+int maxval = 0;
+void setup() {
+  // enable ADC high speed mode  
+  ADCSRA &= ~(bit (ADPS0) | bit (ADPS1) | bit (ADPS2)); // clear prescaler bits
+  ADCSRA |= bit (ADPS0) | bit (ADPS1);                 //   8
+  pinMode(A0, INPUT);
+  pinMode(5, OUTPUT);
+  // set the analog reference (high two bits of ADMUX) and select the
+  // channel (low 4 bits).  this also sets ADLAR (left-adjust result)
+  // to 0 (the default).
+  ADMUX = 192;
+  // set ADMUX as static value for pin 0
+  ADMUX = (AREF << 6) | (0 & 0x07);
+  Serial.begin(115200);
+}
+
+void loop() {
+  if (sample() > ATHRESH) {
+    // spec from volca sample 2 manual: 5V for 15ms
+    PORTD |= B00100000;
+    delay(15);
+    PORTD ^= B00100000;
+  }
+}
